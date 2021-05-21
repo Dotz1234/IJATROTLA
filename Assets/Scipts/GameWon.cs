f471b64f528c40c648ea13c19928026267b64130
Dotzler <connor@mendingwall.com>
2021-05-21 15:28:02 -0700

    new2
    changes

---
+++ Assets/Scipts/GameWon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameWon : MonoBehaviour
+{
+    public GameObject EndScrn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            EndScrn.gameObject.SetActive(true);
+        }
+    }
+    public void Setup(int scrore)
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
+
